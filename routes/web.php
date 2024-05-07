8586fb734b0f96ec392c10b6910f85bacc49ce78
DanielDunevant <ddunevant@VIA-WEBDEV-DT>
2024-05-07 16:03:43 -0500

    initial commit routes and controllers and blade

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [WelcomeController::class,'index']);
+
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('home', ['name'=>"Daniel"]);
+});
